c11688ebbca4b12381e77e992ef1f4d496e48962
Logan <domanimedia@outlook.com>
2023-01-20 15:26:02 -0700

    javascript-practice-1

---
+++ Hello World.js
@@ -0,0 +1,16 @@
+console.log("hello World");
+console.log(true || false);
+console.log(3 % 12);
+
+let cow = "bovine"
+console.log(cow[4]);
+
+let i = 4;
+i + 3;
+console.log(i);
+
+function foo(num) {
+    return num % 3;
+  }
+
+  console.log(foo(9));
